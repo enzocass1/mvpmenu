4031069dbef0b74d36feca83cbc57c95c7963a0d
Enzo Cassese <enzocassese91@gmail.com>
2025-10-15 00:09:23 +0200

    Fix definitivo routing: separa menu pubblico da app

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import PublicMenu from './pages/PublicMenu.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+function Root() {
+  // Controlla se siamo su una route di menu pubblico
+  const isPublicMenuRoute = window.location.hash.startsWith('#/menu/')
+
+  if (isPublicMenuRoute) {
+    // Se siamo su /menu/..., mostra SOLO PublicMenu
+    return (
+      <HashRouter>
+        <Routes>
+          <Route path="/menu/:subdomain" element={<PublicMenu />} />
+        </Routes>
+      </HashRouter>
+    )
+  }
+
+  // Altrimenti mostra l'app normale (login + dashboard)
+  return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/menu/:subdomain" element={<PublicMenu />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
+  )
+}
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Root />
   </StrictMode>,
 )
