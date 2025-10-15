4964d31c7d0b41acdb00db86445e68f53b670dba
Enzo Cassese <enzocassese91@gmail.com>
2025-10-15 17:20:54 +0200

    fix: usa un solo Router in main.jsx per evitare conflitti

--- src/main.jsx
+++ src/main.jsx
@@ -6,50 +6,22 @@ import App from './App.jsx'
 import PublicMenu from './pages/PublicMenu.jsx'
 import ResetPassword from './pages/ResetPassword.jsx'
 
-function Root() {
-  const hash = window.location.hash
-
-  // Route pubbliche che non richiedono autenticazione
-  const isPublicMenuRoute = hash.startsWith('#/menu/')
-  const isResetPasswordRoute = hash.startsWith('#/reset-password')
-
-  if (isPublicMenuRoute) {
-    // Menu pubblico
-    return (
-      <HashRouter>
-        <Routes>
-          <Route path="/menu/:subdomain" element={<PublicMenu />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </HashRouter>
-    )
-  }
-
-  if (isResetPasswordRoute) {
-    // Reset password
-    return (
-      <HashRouter>
-        <Routes>
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </HashRouter>
-    )
-  }
-
-  // App normale (login + dashboard)
-  return (
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <HashRouter>
       <Routes>
+        {/* Route principale - App gestisce login/dashboard */}
         <Route path="/" element={<App />} />
+        
+        {/* Route pubblica - Menu */}
+        <Route path="/menu/:subdomain" element={<PublicMenu />} />
+        
+        {/* Route pubblica - Reset Password */}
+        <Route path="/reset-password" element={<ResetPassword />} />
+        
+        {/* Fallback - redirect a home */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </HashRouter>
-  )
-}
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Root />
   </StrictMode>,
 )
